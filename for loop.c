7d585509b5f7e7f520c37d4fe6a0dd294cddd7cd
Anand VErma <67152953+vermanand1998@users.noreply.github.com>
2021-07-08 10:34:34 +0530

    Create for loop.c

---
+++ for loop.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+
+    int a, b;
+    char* represent[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    scanf("%d\n%d", &a, &b);
+    for(int i = a; i <= b; i++) {
+        
+        if(i <= 9) {
+            printf("%s\n", represent[i]);
+        }
+        else{
+             if(i % 2 == 0)
+                printf("even\n");
+            else printf("odd\n");
+
+        }
+            
+    }
+    return 0;
+}
