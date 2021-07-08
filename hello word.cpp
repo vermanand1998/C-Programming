8db560103e495637c33eb63b760df4cdacba3081
Anand VErma <67152953+vermanand1998@users.noreply.github.com>
2021-07-08 10:27:45 +0530

    Create hello word.cpp

---
+++ hello word.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+printf("Hello, World!");
+    scanf("%[^\n]%*c", &s);
+    printf("\n%s",s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+
+    return 0;
+}
