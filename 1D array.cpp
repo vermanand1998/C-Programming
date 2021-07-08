7ff2e0b5768a9c939173d16deccd4395da6b256a
Anand VErma <67152953+vermanand1998@users.noreply.github.com>
2021-07-08 10:28:30 +0530

    Create 1D array.cpp

---
+++ 1D array.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int *ptr, n, i;
+    int sum=0;
+    scanf("%d", &n);
+    ptr=(int*) malloc(n * sizeof(int));
+    if(ptr==NULL)
+    {
+        exit(EXIT_FAILURE);
+    } 
+    for(i=0; i<n;i++)
+
+    {
+        scanf("%d", ptr+i);
+        sum+=*(ptr+i);
+
+    }  
+    printf("%d", sum);
+    free(ptr);
+    return 0;
+}
